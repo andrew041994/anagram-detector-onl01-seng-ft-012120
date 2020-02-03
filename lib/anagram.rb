21259f20d873d180ee3565b0c35caf2f31db47d5
Alehandro Persaud <andrew041994@gmail.com>
2020-02-03 23:30:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
 
     def match(array)
       array.each do |i|
-        puts i
+        puts i.split("")
         # if i.split("").sort == @word.split("").sort
         # @@matches << i
         # end
